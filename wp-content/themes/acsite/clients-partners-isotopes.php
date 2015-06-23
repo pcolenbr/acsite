8d2fa2de2813686d60619007a28df7393b36c2d8
Pedro Colen <pedro.colen.c.br@gmail.com>
2015-06-23 17:55:52 -0300

    Fixing clients and partners

--- wp-content/themes/acsite/clients-partners-isotopes.php
+++ wp-content/themes/acsite/clients-partners-isotopes.php
@@ -26,47 +26,46 @@
 		</ul>
 	</nav>
 
-
-
 	<div id="clients-partners-isotopes">
 		<?php $args_events = array('post_type' => 'clients_partners', 'posts_per_page' => -1); ?>
 
 		<?php $query = new WP_Query( $args_events ); ?>
 
 		<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+	
+			<?php $client_tag = the_cfc_field( 'clients_partners', 'tag', $post->ID, false, false ); ?>
+			<?php $client_img = the_cfc_field( 'clients_partners', 'background-image', $post->ID, false, false ); ?>
+			<?php $client_description = the_cfc_field( 'clients_partners', 'description', $post->ID, false, false ); ?>
 
-			<?php $client_info = get_post_meta( $post->ID, 'client_info', true ); ?>
-			<?php if(!empty($client_info)): ?>
-				<?php if(get_the_content() != '' ): ?>
-					<a href="<?php the_permalink(' ') ?>">
-					<?php endif; ?>		
+			<?php if(get_the_content() != '' ): ?>
+				<a href="<?php the_permalink(' ') ?>">
+				<?php endif; ?>
 
-					<div class="clients-partners-item <?php echo $client_info[0]["tag"]; ?>">
-						<span class="clients-partners-hover">
-							<span class="table">
-								<span class="middle">
-									<?php if (!empty($client_info[0]["description"])){ ?>
-									<p class="description"><?php echo $client_info[0]["description"]; ?></p>
-									<?php } ?>
-								</span>
+				<div class="clients-partners-item <?php echo $client_tag; ?>">
+					<span class="clients-partners-hover">
+						<span class="table">
+							<span class="middle">
+								<?php if (!empty($client_description)){ ?>
+								<p class="description"><?php echo $client_description ?></p>
+								<?php } ?>
 							</span>
 						</span>
+					</span>
 
-						<?php $background_src = wp_get_attachment_image_src( $client_info[0]['background-image'], 'full' ); ?>
-						<img src="<?php echo $background_src[0]; ?>" alt="">
-					</div>
+					<?php $background_src = wp_get_attachment_image_src( $client_info[0]['background-image'], 'full' ); ?>
+					<img src="<?php echo $client_img; ?>" alt="">
+				</div>
 
-					<?php if(get_the_content() != '' ): ?>
-					</a>	
-				<?php endif; ?>
+				<?php if(get_the_content() != '' ): ?>
+				</a>	
 			<?php endif; ?>
 
 		<?php endwhile; ?>
 
-	<?php else : ?>
+		<?php else : ?>
 
-		<h2>No Clients and Partners</h2>
+			<h2>No Clients and Partners</h2>
 
-	<?php endif; ?>
-</div>
+		<?php endif; ?>
+	</div>
 </section>
